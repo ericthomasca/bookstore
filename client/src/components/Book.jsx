be9d1b534b8aad87574e81a87ad3c628bd3f8870
Eric Thomas <eric@ericthomas.ca>
2022-10-06 22:41:53 -0230

    prepare for refactoring

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -1,20 +1,61 @@
-import Container from 'react-bootstrap/Container';
+import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
 
 function Book() {
 
-    let book = "Fake Book";
-    let author = "Mrs. Fake";
-    let publish_year = "1987";
-    let cover_image = "TEST IMAGE";
+  const baseURL = "https://openlibrary.org";
 
-    return (
-        <Container fluid>
-            <h3>{book}</h3>
-            <p>{author}</p>
-            <p>{publish_year}</p>
-            <p>{cover_image}</p>
-        </Container>
-    );
+  // TODO get from search later
+  let bookSuffix = "/works/OL45804W";
+
+  // Book Stuff
+  // TODO move to another file
+  let bookURL = baseURL + bookSuffix + ".json";
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState(0);
+  const [coverImage, setCoverImage] = useState(0);
+
+
+  async function fetchBookData(url) {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setTitle(json.title);
+      setAuthor(json.authors[0].author.key);
+      setPublishYear(1983);
+      setCoverImage(json.covers[0]);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+  fetchBookData(bookURL);
+
+
+
+  // async function fetchAuthorData(url) {
+  //   try {
+  //     const response = await fetch(url);
+  //     const json = await response.json();
+  //     setTitle(json.title);
+  //     setAuthor(json.authors[0].author.key);
+  //     setPublishYear(1983);
+  //     setCoverImage(json.covers[0]);
+  //   } catch (error) {
+  //     console.log("Error", error);
+  //   }
+  // }
+  // fetchAuthorData(url);
+
+  return (
+    <Container fluid>
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <p>{publishYear}</p>
+      <p>{coverImage}</p>
+    </Container>
+  );
 }
 
 export default Book;
